d64a0cef31766463c68bda4527c4343279a2e67f
saint-preacher <idk5138@gmail.com>
2021-03-24 23:28:43 +0400

    Added Task_2.1._OOP_okay-okay_Programming
    Includes tasks 2.1.1 and 2.1.2

---
+++ Task_2/Task_2.1._OOP_okay-okay_Programming/Task_2.1._OOP_okay-okay_Programming/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_2._1._OOP_okay_okay_Programming
+{
+    /// <summary>
+    /// Class Line allows user to create a line with two pairs of coordinates (x1, y1); (x2, y2);
+    /// Added method to count length of the line.
+    /// </summary>
+    public class Line : Point
+    {
+        public Line(double x1, double y1, double x2, double y2, string name) : base(x1, y1, name)
+        {
+            X2 = x2;
+            Y2 = y2;
+        }
+
+        public double X2 { get; }
+        public double Y2 { get; }
+
+        public virtual double Length => Math.Sqrt((X2 - X) * (X2 - X) + (Y2 - Y) * (Y2 - Y));
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($"Pair of coordinates is: ({X2}, {Y2})");
+            Console.WriteLine($"Length of the {Name} is: {Length}");
+        }
+    }
+}
